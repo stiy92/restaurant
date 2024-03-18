adb409131dd27a25f79df82ac88eebe496c1a551
stiy92 <stiy92@hotmail.com>
2024-03-18 10:57:31 -0500

    categoria modificada en tabla producto

--- ajax/datatable-productos.ajax.php
+++ ajax/datatable-productos.ajax.php
@@ -139,10 +139,10 @@ class TablaProductos{
  	 		TRAEMOS LA CATEGORÍA
   			=============================================*/ 
 
-		  	$item = "id";
-		  	$valor = $productos[$i]["id_categoria"];
+		  	// $item = "id";
+		  	// $valor = $productos[$i]["id_categoria"];
 
-		  	$categorias = ControladorCategorias::ctrMostrarCategorias($item, $valor);
+		  	// $categorias = ControladorCategorias::ctrMostrarCategorias($item, $valor);
 
 		  	/*=============================================
  	 		STOCK
@@ -184,7 +184,6 @@ class TablaProductos{
 			      "'.$imagen.'",
 			      "'.$productos[$i]["codigo"].'",
 			      "'.$productos[$i]["descripcion"].'",
-			      "'.$categorias["categoria"].'",
 			      "'.$stock.'",
 			      "'.$productos[$i]["precio_compra"].'",
 			      "'.$productos[$i]["precio_venta"].'",
